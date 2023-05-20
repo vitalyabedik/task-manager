46610e5309179d9bf5fcdd9cc2684031104d809f
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-20 23:31:04 +0300

    feat: finish 08-lesson (reducer, unit test)

---
+++ src/state/todolists-reducer.test.ts
@@ -0,0 +1,74 @@
+import {v1} from 'uuid';
+import {FilterType, TodolistType} from '../App';
+import {
+    addTodolistAC, changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    removeTodolistAC,
+    todolistsReducer
+} from './todolists-reducer';
+
+test('correct todolist should be removed', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    const startState: TodolistType[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+
+    const endState = todolistsReducer(startState, removeTodolistAC(todolistId1))
+
+    expect(endState.length).toBe(1)
+    expect(endState[0].id).toBe(todolistId2)
+})
+
+test('correct todolist should be added', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    let newTodolistTitle = 'newTodolistTitle'
+
+    const startState: TodolistType[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+
+    const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle))
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
+})
+
+test('correct todolist should changed title', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    let newTodolistTitle = 'newTodolistTitle'
+
+    const startState: TodolistType[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+
+    const endState = todolistsReducer(startState, changeTodolistTitleAC(todolistId2, newTodolistTitle))
+
+    expect(endState[0].title).toBe('What to learn')
+    expect(endState[1].title).toBe(newTodolistTitle)
+})
+
+test('correct filter should be changed', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    let newFilter: FilterType = 'completed'
+
+    const startState: TodolistType[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+
+    const endState = todolistsReducer(startState, changeTodolistFilterAC(todolistId2, newFilter))
+
+    expect(endState[0].filter).toBe('all')
+    expect(endState[1].filter).toBe(newFilter)
+})
---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,72 @@
+import {FilterType, TodolistType} from '../App';
+import {v1} from 'uuid';
+
+export const todolistsReducer = (state: TodolistType[], action: TodolistsActionsType) => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST' : {
+            return state.filter(t => t.id !== action.payload.todolistId)
+        }
+        case 'ADD-TODOLIST': {
+            const newTodolist = {
+                id: v1(),
+                title: action.payload.newTitle,
+                filter: 'all'
+            }
+            return [...state, newTodolist]
+        }
+        case 'CHANGE-TODOLIST-TITLE': {
+            return state.map(t => t.id === action.payload.todolistId ? {...t, title: action.payload.newTitle} : t)
+        }
+        case 'CHANGE-TODOLIST-FILTER': {
+            return state.map(t => t.id === action.payload.todolistId ? {...t, filter: action.payload.newFilter} : t)
+        }
+
+        default:
+            throw new Error("I don't understand this action type")
+    }
+}
+
+
+export type TodolistsActionsType = RemoveTodolistACType | AddTodolistACType | ChangeTodolistTitleAC | ChangeTodolistFilterACType
+
+type RemoveTodolistACType = ReturnType<typeof removeTodolistAC>
+export const removeTodolistAC = (todolistId: string) => {
+    return {
+        type: 'REMOVE-TODOLIST',
+        payload: {
+            todolistId
+        }
+    } as const
+}
+
+type AddTodolistACType = ReturnType<typeof addTodolistAC>
+export const addTodolistAC = (newTitle: string) => {
+    return {
+        type: 'ADD-TODOLIST',
+        payload: {
+            newTitle
+        }
+    } as const
+}
+
+type ChangeTodolistTitleAC = ReturnType<typeof changeTodolistTitleAC>
+export const changeTodolistTitleAC = (todolistId : string, newTitle: string) => {
+    return {
+        type: 'CHANGE-TODOLIST-TITLE',
+        payload: {
+            todolistId,
+            newTitle
+        }
+    } as const
+}
+
+type ChangeTodolistFilterACType = ReturnType<typeof changeTodolistFilterAC>
+export const changeTodolistFilterAC = (todolistId : string, newFilter: FilterType) => {
+    return {
+        type: 'CHANGE-TODOLIST-FILTER',
+        payload: {
+            todolistId,
+            newFilter
+        }
+    } as const
+}
