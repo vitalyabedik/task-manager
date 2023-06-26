48ddc59cb6198a44b9252a58c62e88a0833f83d7
Vitali Bedik <vitalya.bedik@gmail.com>
2023-06-26 23:16:14 +0300

    feat: finish 13-lesson (refactoring with tests)

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -84,7 +84,7 @@ test('correct task should be added to correct array', () => {
     expect(endState['todolistId2'].length).toBe(4)
     expect(endState['todolistId2'][0].id).toBeDefined()
     expect(endState['todolistId2'][0].title).toBe('juce')
-    expect(endState['todolistId2'][0].status).toBe(TaskStatuses.New)
+    expect(endState['todolistId2'][0].status).toBe(TaskStatuses.Completed)
 })
 
 test('status of specified task should be changed', () => {
