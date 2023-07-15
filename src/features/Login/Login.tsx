8f5c1164cd9c687006c0fa5d044ca26a4b0d08b7
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-15 15:30:43 +0300

    feat: form validation with yup

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 import Grid from '@mui/material/Grid';
 import Checkbox from '@mui/material/Checkbox';
@@ -23,21 +24,12 @@ export const Login = () => {
             password: '',
             rememberMe: false
         },
-        validate: (values) => {
-            const errors: FormikErrorType = {}
-            if (!values.email) {
-                errors.email = 'Required'
-            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                errors.email = 'Invalid email address'
-            }
-
-            if (!values.password) {
-                errors.password = 'Required'
-            } else if (values.password.length <= 3) {
-                errors.password = 'Must be 3 characters or more'
-            }
-            return errors
-        },
+        validationSchema: Yup.object({
+            email: Yup.string().email('Invalid email address').required('Required'),
+            password: Yup.string()
+                .min(3, 'Password must be 3 characters or more')
+                .required('Required'),
+        }),
         onSubmit: values => {
             alert(JSON.stringify(values));
             formik.resetForm()
