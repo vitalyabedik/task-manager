9eed63e8ac2932b6bbcba673767299ee55865e50
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-16 19:50:01 +0300

    fix: change types for store, thunks, create hook

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
-import thunk, {ThunkDispatch} from 'redux-thunk';
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 
 import {tasksReducer} from '../features/TodolistsList/Task/tasks-reducer'
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer'
@@ -16,11 +16,19 @@ const rootReducer = combineReducers({
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
-export type AppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
-export const useAppDispatch = () => useDispatch<AppDispatchType>();
+export type RootReducerType = ReturnType<typeof rootReducer>
+export type RootStateType = ReturnType<typeof store.getState>
+export type AppDispatch = ThunkDispatch<RootStateType, unknown,AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootStateType,
+    unknown,
+    AnyAction
+>
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
+
+
+
