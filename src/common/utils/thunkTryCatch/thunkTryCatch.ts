9f1b7f2e19e330a8c57eb3798201d5e98085c094
Vitali Bedik <vitalya.bedik@gmail.com>
2023-08-03 18:58:25 +0300

    feat: create util function thunkTryCatch and JS doc

---
+++ src/common/utils/thunkTryCatch/thunkTryCatch.ts
@@ -0,0 +1,31 @@
+import { AppDispatch, AppRootStateType } from 'app/store';
+import { handleServerNetworkError } from 'common/utils/errors';
+import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk';
+import { appActions } from 'app/app.reducer';
+import { ResponseType } from 'common/api/common.types.api';
+
+/**
+ * A utility function that wraps asynchronous logic in a try-catch block with specific error handling.
+ *
+ * @template T - The type of the expected result from the asynchronous logic.
+ * @param {BaseThunkAPI<AppRootStateType, any, AppDispatch, null | ResponseType>} thunkAPI - The base thunk API provided by Redux Toolkit.
+ * @param {() => Promise<T>} logic - The asynchronous function that contains the logic to be executed in the try block.
+ * @returns {Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>>} A promise that resolves with the result of the logic if successful,
+ *    or rejects with a specific value from `thunkAPI.rejectWithValue` if an error occurs.
+ */
+
+const thunkTryCatch = async <T>(
+  thunkAPI: BaseThunkAPI<AppRootStateType, any, AppDispatch, null | ResponseType>,
+  logic: () => Promise<T>,
+): Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>> => {
+  const { dispatch, rejectWithValue } = thunkAPI
+  dispatch(appActions.setAppStatus({ status: "loading" }))
+  try {
+    return await logic()
+  } catch (e) {
+    handleServerNetworkError(dispatch, e)
+    return rejectWithValue(null)
+  } finally {
+    dispatch(appActions.setAppStatus({ status: "idle" }))
+  }
+}
