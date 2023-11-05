065113316c03fa19e8b745c23f6d1e718aab112a
Vitali Bedik <vitalya.bedik@gmail.com>
2023-11-05 13:02:47 +0300

    fix: for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-import "./index.css"
+import './index.css';
 
-import { App } from "app/ui/app"
-import { store } from "app/model/store"
+import { App } from 'app/ui/app';
+import { store } from 'app/model/store';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter basename="/it-incubator.todolist-learning">
+  <BrowserRouter basename='/task-manager'>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
-)
+);
