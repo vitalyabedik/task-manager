82fa5397aceed58b2d312577f56681452fc10ab3
Vitali Bedik <vitalya.bedik@gmail.com>
2024-01-04 21:28:44 +0300

    feat: changer header content

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,43 +1,38 @@
-import React from "react"
+import React from 'react';
 
-import AppBar from "@mui/material/AppBar"
-import Toolbar from "@mui/material/Toolbar"
-import IconButton from "@mui/material/IconButton"
-import Menu from "@mui/icons-material/Menu"
-import Typography from "@mui/material/Typography"
-import Button from "@mui/material/Button"
-import { Logout } from "@mui/icons-material"
-import { LinearProgress } from "@mui/material"
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Logout } from '@mui/icons-material';
+import { LinearProgress } from '@mui/material';
 
-import { useActions, useAppSelector } from "common/hooks"
-import { RequestStatusType } from "app/model/app.slice"
-import { selectAuthIsLoggedIn } from "features/auth/model/auth.selectors"
-import { authThunks } from "features/auth/model/auth.slice"
+import { useActions, useAppSelector } from 'common/hooks';
+import { RequestStatusType } from 'app/model/app.slice';
+import { selectAuthIsLoggedIn } from 'features/auth/model/auth.selectors';
+import { authThunks } from 'features/auth/model/auth.slice';
 
 export const Header = (): JSX.Element => {
-  const status = useAppSelector<RequestStatusType>((state) => state.app.status)
-  const isLoggedIn = useAppSelector(selectAuthIsLoggedIn)
+  const status = useAppSelector<RequestStatusType>((state) => state.app.status);
+  const isLoggedIn = useAppSelector(selectAuthIsLoggedIn);
 
-  const { logout } = useActions(authThunks)
+  const { logout } = useActions(authThunks);
 
-  const onLogoutHandler = () => logout()
+  const onLogoutHandler = () => logout();
 
   return (
-    <AppBar position="static">
+    <AppBar position='static'>
       <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <Menu />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          News
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          Task manager
         </Typography>
         {isLoggedIn && (
-          <Button onClick={onLogoutHandler} color="inherit">
+          <Button onClick={onLogoutHandler} color='inherit'>
             <Logout />
           </Button>
         )}
       </Toolbar>
-      {status === "loading" && <LinearProgress />}
+      {status === 'loading' && <LinearProgress />}
     </AppBar>
-  )
-}
+  );
+};
